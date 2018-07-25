2beb7957db49ab9db0e2427a4d811bc031808e5a
NGA <40926806+ksh95216@users.noreply.github.com>
2018-07-25 21:52:50 +0900

    Update gpt.py

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -13,14 +13,14 @@ def parsing(f):
 			parse="0x"+data[512+72:512+80][::-1].hex()
 			parse=int(parse,16)/2
 			parse=round(parse)
-			print(parse)
+
 			data = read_sectors(f, parse, parse+1)
 			idx=0
 			while(data[32] != 0):
 				p=int((data[32:40][::-1].hex()),16)*512
 				p1=int((data[40:48][::-1].hex()),16)*512
 			
-				print("Partition {} start: {} End: {} Size: {}".format(idx+1,hex(p), hex(p1),hex(p1-p+(1*512
+				print("Partition {} start: {} Size: {}".format(idx+1,hex(p),hex(p1-p+(1*512
 				))))
 				data=data[128:]
 				idx+=1
